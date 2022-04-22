36b623cf976e7ea130a370e5a7bff4fe3e3b2e23
Philip Smolen <philip+github@trade-ideas.com>
2022-04-22 01:00:33 -0700

    There is now a boucing ball!
    Version 1.0!

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,12 @@ import { Options } from "roughjs/bin/core";
 const boxMax = 14;
 const boxMin = -boxMax;
 const boxSize = boxMax - boxMin;
-const ballRadius = 0.5;
+const ballRadius = 1.5;
 // The _center_ of the ball can _not_ go all the way to the wall.
 // Always reserve one ball radius.
 const ballMin = boxMin + ballRadius;
 const ballMax = boxMax - ballRadius;
+const ballRange = ballMax - ballMin;
 
 type Point3 = {
   /**
@@ -95,7 +96,7 @@ function wallOptions(color: string): Options {
     disableMultiStrokeFill: true,
     hachureAngle: Math.random() * 360,
     //preserveVertices: true,
-    };
+  };
 }
 
 const left = roughSvg.polygon(
@@ -156,3 +157,87 @@ svgBackground.appendChild(back);
 //const y = roughSvg.polygon([[5, 5], [5, 95], [95, 95],[95, 5]]);
 //svgBackground.appendChild(y)
 
+function makeBall(center: Point3) {
+  const diameter = ballRadius * flattenRatio(center.z) * 2;
+  const [x, y] = flatten(center);
+  const main = roughSvg.circle(x, y, diameter, {
+    fill: "#ffa0a0",
+    stroke: "none",
+    strokeWidth: 0.2,
+    disableMultiStroke: true,
+    fillStyle: "solid",
+    roughness:0.3333,
+    // curveStepCount:3 looks a lot like a triangle, sorta
+    //curveStepCount:99 looks like a paintball hit something.  
+  });
+  svgForeground.appendChild(main);
+  return main;
+}
+
+/*
+(window as any).makeBall = makeBall;
+const numberOfBalls = 5;
+const positionMaker = makeLinear(0, ballMin, numberOfBalls - 1, ballMax);
+for (let i = 0; i < numberOfBalls; i++) {
+  const position = positionMaker(i);
+  makeBall({ x: position, y: position, z: position });
+}
+*/
+
+const ballPosition : Point3 = { x: Math.random() * ballRange + ballMin, y: Math.random() * ballRange + ballMin, z: Math.random() * ballRange + ballMin,  };
+const ballVelocity = {
+  x: Math.random() * 100 - 50,
+  y: Math.random() * 100 - 50,
+  z: Math.random() * 100 - 50,
+};
+let lastBallUpdate: number | undefined;
+
+function updateBall(time: DOMHighResTimeStamp) {
+  if (lastBallUpdate !== undefined) {
+    const secondsPassed = (time - lastBallUpdate) / 1000;
+    if (secondsPassed <= 0) {
+      // Do NOT update lastBallUpdate.  Do not do anything.
+      // The logic for bouncing might not work well in reverse.
+      return;
+    }
+    ballPosition.x += ballVelocity.x * secondsPassed;
+    if (ballPosition.x < ballMin) {
+      ballPosition.x = ballMin;
+      ballVelocity.x = Math.abs(ballVelocity.x);
+    } else if (ballPosition.x > ballMax) {
+      ballPosition.x = ballMax;
+      ballVelocity.x = -Math.abs(ballVelocity.x);
+    }
+    ballPosition.y += ballVelocity.y * secondsPassed;
+    if (ballPosition.y < ballMin) {
+      ballPosition.y = ballMin;
+      ballVelocity.y = Math.abs(ballVelocity.y);
+    } else if (ballPosition.y > ballMax) {
+      ballPosition.y = ballMax;
+      ballVelocity.y = -Math.abs(ballVelocity.y);
+    }
+    ballPosition.z += ballVelocity.z * secondsPassed;
+    if (ballPosition.z < ballMin) {
+      ballPosition.z = ballMin;
+      ballVelocity.z = Math.abs(ballVelocity.z);
+    } else if (ballPosition.z > ballMax) {
+      ballPosition.z = ballMax;
+      ballVelocity.z = -Math.abs(ballVelocity.z);
+    }
+  }
+  lastBallUpdate = time;
+}
+
+let ballSvg : SVGElement | undefined;
+
+function animate(time: DOMHighResTimeStamp) {
+  if (ballSvg) {
+    ballSvg.remove();
+    ballSvg = undefined;
+  }
+  requestAnimationFrame(animate);
+  updateBall(time);
+  ballSvg = makeBall(ballPosition);
+  svgForeground.appendChild(ballSvg);
+}
+requestAnimationFrame(animate);
